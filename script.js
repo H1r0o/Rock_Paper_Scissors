11ed2a6d606747cdf58be008ebcfb0711a1a2032
Daniel Gradonski <daniel.gradonski@gmail.com>
2024-12-19 20:27:43 +0100

    Dodanie mechaniki do rozegrania jednej rundy

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let computerChoiceResault = "";
-let playerChoiceResault = "";
+// let computerChoiceResault = "";
+// let playerChoiceResault = "";
 
 let playerScore = 0;
 let computerScore = 0;
 
 function computerChoice() {
     let random = Math.floor(Math.random() * 3);
-
+    let computerChoiceResault;
     if (random == 0) {
         computerChoiceResault = "papier";
     } else if (random == 1) {
@@ -14,11 +14,26 @@ function computerChoice() {
     } else if (random == 2) {
         computerChoiceResault = "nożyce";
     }
-    console.log(computerChoiceResault);
+    return computerChoiceResault;
 }
 
 function playerChoice() {
-    playerChoiceResault = prompt("Co wybierasz?");
+    let playerChoiceResault = prompt("Co wybierasz?");
     playerChoiceResault = playerChoiceResault.toLocaleLowerCase();
-    console.log(playerChoiceResault);
+    return playerChoiceResault;
 }
+
+function playRound(player, computer) {
+    console.log(`Gracz wybrał ${player} Komputer wybrał ${computer}`)
+    if (player === computer) {
+        console.log("remis");
+    } else if (player === "papier" && computer === "kamień" || player === "kamień" && computer === "nożyce" || player === "nożyce" && computer === "papier") {
+        console.log("Wygrywa gracz");
+        playerScore++;
+    } else if (player === "kamień" && computer === "papier" || player === "nożyce" && computer === "kamień" || player === "papier" && computer === "nożyce") {
+        console.log("Wygrywa komputer");
+        computerScore++;
+    }
+}
+
+playRound(playerChoice(), computerChoice());
